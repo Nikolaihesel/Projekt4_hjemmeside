fa16ca9acdde42a87d865ee9a0cedd6c37d50b8c
Nikolaihesel <77057340+Nikolaihesel@users.noreply.github.com>
2021-09-22 10:22:38 +0200

    form er lagt ind, ikke positioneret

--- app.js
+++ app.js
@@ -20,3 +20,27 @@ function showMenu() {
     };
 };
 
+let modal = document.getElementById('bg_blur_confirm');
+let close = document.getElementById('close');
+let contactInfo = []
+function showInfo() {
+    let myName = document.getElementById("fname").value;
+    document.getElementById("display_fname").innerHTML = myName;
+    let myLastName = document.getElementById("lname").value;
+    document.getElementById("display_lname").innerHTML = myLastName;
+    let myDate = document.getElementById("dateInfo").value;
+    document.getElementById("display_date").innerHTML = myDate;
+    let myEmail = document.getElementById("emailInfo").value;
+    document.getElementById("display_email").innerHTML = myEmail;
+    modal.style.display = "block";
+}
+
+ // stop siden fra a reloade ved submit
+ var form = document.getElementById("form");
+
+ function handleForm(event) {
+      event.preventDefault(); } 
+
+ form.addEventListener('submit', handleForm);
+
+
