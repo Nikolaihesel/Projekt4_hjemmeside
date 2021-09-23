64e0f4cc5a02670eb0d3cafd366278bc6ac43f17
Tanjaronnemose <taro32181@edu.ucl.dk>
2021-09-23 18:26:21 +0200

    JS + css på popup

---
+++ app_tanja.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+const popupScreen = document.querySelector(".popup-screen");
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    popupScreen.classList.add("active");
+  }, 2000)
+});
+</script>
