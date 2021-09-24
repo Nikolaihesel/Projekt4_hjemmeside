caa3405681be53949c22165f270484d4b945b56d
Tanjaronnemose <taro32181@edu.ucl.dk>
2021-09-24 09:49:43 +0200

    Rettelser popup

--- app_tanja.js
+++ app_tanja.js
@@ -1,9 +1,13 @@
-<script type="text/javascript">
 const popupScreen = document.querySelector(".popup-screen");
+const popupBox = document.querySelector(".popup-box");
+const closeBtn = document.querySelector(".close-btn");
 
 window.addEventListener("load", () => {
   setTimeout(() => {
     popupScreen.classList.add("active");
-  }, 2000)
+  }, 2000); //Popup the screen in 02 seconds after the page is loaded
 });
-</script>
+
+closeBtn.addEventListener("click", () => {
+  popupScreen.classList.remove("active"); //Close the popup screen on click the close button
+})
