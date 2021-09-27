6e6e5241b6420fb69dfa154d305eea6857166552
LauraLorentzen <lauramlorentzen@gmail.com>
2021-09-27 15:25:05 +0200

    bogstav

--- app_tanja.js
+++ app_tanja.js
@@ -20,7 +20,7 @@ window.addEventListener("load", () => {
 
 
 function popUpText() {
-  let popUpInfo = ["Vidste du, at du kan få hjælp til at klare din hverdag som studerende og undgå unødvendig stress?", 'Book til til en studievejleder, stresscoach, psykoterapeup eller psykolog - det er helt gratis.']
+  let popUpInfo = ["Vidste du, at du kan få hjælp til at klare din hverdag som studerende og undgå unødvendig stress?", 'Book tid til en studievejleder, stresscoach, psykoterapeup eller psykolog - det er helt gratis.']
 
 
   const menu = document.getElementById('popup_text');
