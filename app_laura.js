bdf4535c7c326f57a941a6da822339f441adc3fe
LauraLorentzen <lauramlorentzen@gmail.com>
2021-09-27 13:29:24 +0200

    k til c

--- app_laura.js
+++ app_laura.js
@@ -1,11 +1,11 @@
-dokument.getElementById("open-popup-btn").addEventListener("click",function(){
-  dokument.getElementByClassName("popup")[0].classList.add("active");
+document.getElementById("open-popup-btn").addEventListener("click",function(){
+  document.getElementByClassName("popup")[0].classList.add("active");
 });
 
-dokument.getElementById("dissmiss-popup-btn").addEventListener("click",function(){
-  dokument.getElementByClassName("popup")[0].classList.remove("active");
+document.getElementById("dissmiss-popup-btn").addEventListener("click",function(){
+  document.getElementByClassName("popup")[0].classList.remove("active");
 });
 
-dokument.getElementById("samarbejde").addEventListener("click",function(){
+document.getElementById("samarbejde").addEventListener("click",function(){
   alert();
 });
